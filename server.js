5aad6579b5b6d0981cdf671559f6857b217736d7
Richard Hong <rich.hong@gmail.com>
2016-05-23 12:32:50 -0700

    Added user counter

--- server.js
+++ server.js
@@ -6,14 +6,26 @@ var port = process.env.PORT || 5000;
 
 app.use(express.static(__dirname + '/public'));
 
+//Sets variable to store number of users
+var connectCounter = 0;
+
 //Initialize socket on connection
 io.on('connection', function(socket){
+
+  //Increase counter on connection and sends to client
+  connectCounter++;
+  io.emit('connectCounter', connectCounter);
 
   //Event handler to emit message when chat message comes in
   socket.on('chat message', function(message){
     io.emit('chat message', message);
   });
 
+  //Event handler that reduces count when disconnected
+  socket.on('disconnect', function(){
+    connectCounter--;
+    io.emit('connectCounter', connectCounter);
+  });
 });
 
 http.listen(port, function(){
